94cf027d432b0a9a09729c8811df3f4fc6a8ef47
Arturs Priede <arturs.priede88@gmail.com>
2023-09-28 22:38:33 +0300

    LMR - Late Move Reduction (#47)
    * feat: Late Move Recution
    
    * feat(lmr): full research on fail
    
    * feat(lmr): increased reuction
    
    * feat(lmr): remove legal move dependency
    
    * feat(lmr): start lmr after move 1
    
    * feat(lmr): decrease depth threshold
    
    * feat(lmr): add lmr, add golangci

--- pkg/evaluation/alphabeta.go
+++ pkg/evaluation/alphabeta.go
@@ -97,10 +97,19 @@ func (e *EvalEngine) PVS(ctx context.Context, pvOrder []board.Move, line *[]boar
 			if legalMoves == 1 {
 				value = -e.PVS(ctx, pvOrder, &pv, depth-1, ply+1, -beta, -alpha, true, -side)
 			} else {
-				value = -e.PVS(ctx, pvOrder, &pv, depth-1, ply+1, -(alpha + 1), -alpha, true, -side)
-				if value > alpha {
+
+				depthR := int8(0)
+				if !isPV && legalMoves > 4 && !inCheck && depth > 3 &&
+					currMove.Promotion() == 0 && !currMove.IsEnPassant() && board.SquareBitboards[currMove.To()]&e.Board.Occupancy[board.BOTH] == 0 {
+					depthR = max(2, depth/4) + int8(legalMoves)/8
+				}
+
+				value = -e.PVS(ctx, pvOrder, &pv, depth-1-depthR, ply+1, -(alpha + 1), -alpha, true, -side)
+
+				if value > alpha && value < beta {
 					value = -e.PVS(ctx, pvOrder, &pv, depth-1, ply+1, -beta, -alpha, true, -side)
 				}
+
 			}
 			umove()
 			e.RemovePly()
